9928dd702c8288ca4d122a0fb485e8fe457319b8
Tarlochan5268 <brotherstarlochan5268@gmail.com>
2020-06-02 19:29:38 -0400

    Product class made

--- src/Product.java
+++ src/Product.java
@@ -1,2 +1,48 @@
 public class Product {
+    int no;
+    String name;
+    int cost;
+    int stock;
+
+    public Product(int no, String name, int cost, int stock) {
+        this.no = no;
+        this.name = name;
+        this.cost = cost;
+        this.stock = stock;
+    }
+
+    public Product() {
+    }
+
+    public int getNo() {
+        return no;
+    }
+
+    public void setNo(int no) {
+        this.no = no;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
 }
