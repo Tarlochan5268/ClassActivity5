302aacff914d5614b3d94355e6e83c3d2d10d458
Tarlochan5268 <brotherstarlochan5268@gmail.com>
2020-06-02 19:55:53 -0400

    Application class made

---
+++ src/Application.java
@@ -0,0 +1,23 @@
+public class Application {
+    Product products[];
+
+    public Application() {
+        products = new Product[]{
+                new Product(1,"Mouse",10,2),
+                new Product(2,"Keyboard",100,5),
+                new Product(3,"USB",50,10),
+                new Product(4,"Headset",150,5),
+                new Product(5,"CPU",250,0),
+
+        };
+    }
+
+    double findPrice(String name)
+    {
+        for (Product mProduct:products) {
+            if(mProduct.getName().equalsIgnoreCase(name))
+                return mProduct.getPrice();
+
+        }
+    }
+}
