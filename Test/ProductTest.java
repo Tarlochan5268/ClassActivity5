94def47deb9bd793d749a56ba76ffa0f2a0ee4e0
Tarlochan5268 <brotherstarlochan5268@gmail.com>
2020-06-02 20:05:03 -0400

    added Products to Test Product

--- Test/ProductTest.java
+++ Test/ProductTest.java
@@ -4,6 +4,11 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ProductTest {
 
+    Product product1 = new Product(1,"Mouse",10,2);
+    Product product2 = new Product(2,"Keyboard",100,5);
+    Product product3 = new Product(3,"USB",50,10);
+    Product product4 = new Product(4,"Headset",150,5);
+    Product product5 = new Product(5,"CPU",250,0);
     @Test
     void testGetPrice()
     {
