89fcece608b7593b5d2c7b50b1ca9fd682eff708
Tarlochan5268 <brotherstarlochan5268@gmail.com>
2020-06-02 20:43:00 -0400

    Application Test Class Completed

--- Test/ApplicationTest.java
+++ Test/ApplicationTest.java
@@ -1,5 +1,31 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ApplicationTest {
+    Product products[] = new Product[]{
+        new Product(1,"Mouse",10,2),
+                new Product(2,"Keyboard",100,5),
+                new Product(3,"USB",50,11),
+                new Product(4,"Headset",150,5),
+                new Product(5,"CPU",250,0),
+    };
+    Application mapplication = new Application(products);
+
+    @Test
+    void testFindPrice()
+    {
+        assertEquals(14.5,mapplication.findPrice("Mouse"));
+        assertEquals(123.5,mapplication.findPrice("Keyboard"));
+        assertEquals(66.825,mapplication.findPrice("USB"));
+        assertEquals(185.25,mapplication.findPrice("Headset"));
+        assertEquals(308.75,mapplication.findPrice("cpu"));
+    }
+
+    @Test
+    void testInStockProducts()
+    {
+        assertEquals(4,mapplication.inStockProducts());
+    }
 
 }
--- src/Application.java
+++ src/Application.java
@@ -4,16 +4,11 @@ public class Application {
     Product products[];
 
     public Application() {
-        products = new Product[]{
-                new Product(1,"Mouse",10,2),
-                new Product(2,"Keyboard",100,5),
-                new Product(3,"USB",50,10),
-                new Product(4,"Headset",150,5),
-                new Product(5,"CPU",250,0),
-
-        };
     }
 
+    public Application(Product[] products) {
+        this.products = products;
+    }
 
     double findPrice(String name)
     {
